a19bf0422ad7f841060b209979e5ec5dc0dfea9f
MQ <mq@MQs-MacBook-Pro.local>
2021-04-15 11:18:54 +0300

    making sure everything works and running on the mac

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,21 +1,19 @@
 package ie.tudublin;
 
 import example.CubeVisual;
+import example.CubeVisual1;
 import example.MyVisual;
 import example.RotatingAudioBands;
 
-public class Main
-{	
+public class Main {
 
-	public void startUI()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new MyVisual());		
-	}
+  public void startUI() {
+    String[] a = { "MAIN" };
+    processing.core.PApplet.runSketch(a, new RotatingAudioBands());
+  }
 
-	public static void main(String[] args)
-	{
-		Main main = new Main();
-		main.startUI();			
-	}
+  public static void main(String[] args) {
+    Main main = new Main();
+    main.startUI();
+  }
 }
